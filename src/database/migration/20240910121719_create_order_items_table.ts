2d9ca53f42edad38ea05117b371c7aa18e138078
Safal raj Manandhar <safalm74@gmail.com>
2024-09-10 18:09:22 +0545

    migrate order_items table

---
+++ src/database/migration/20240910121719_create_order_items_table.ts
@@ -0,0 +1,48 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = "order_items";
+
+/**
+ * Create table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.uuid("id").defaultTo(knex.raw("gen_random_uuid()")).primary();
+    table.uuid("order_id").notNullable().references("id").inTable("orders");
+    table
+      .uuid("grocery_id")
+      .notNullable()
+      .references("id")
+      .inTable("groceries");
+    table.integer("quantity").notNullable();
+    table.integer("price_per_unit").notNullable();
+    table.timestamp("created_at").notNullable().defaultTo(knex.raw("now()"));
+    table
+      .uuid("created_by")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users");
+    table.timestamp("updated_at").nullable();
+    table
+      .uuid("updated_by")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .nullable();
+    table.timestamp("deleted_at").nullable();
+  });
+}
+
+/**
+ * Drop table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
