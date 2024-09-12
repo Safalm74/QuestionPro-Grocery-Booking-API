a8cc55582aba32b860f4161f9afec9558e46b9f7
Safal raj Manandhar <safalm74@gmail.com>
2024-09-12 17:48:49 +0545

    add api docs for orderitem api

--- src/controllers/orderItem.ts
+++ src/controllers/orderItem.ts
@@ -1,10 +1,52 @@
 import * as OrderItemService from "../services/orderItem";
 import { Response, NextFunction } from "express";
 import { Request } from "../interfaces/auth";
-import { IOrderItems } from "../interfaces/orderItem";
 import HttpStatusCode from "http-status-codes";
 import { UUID } from "crypto";
 
+/**
+ * @swagger
+ * /order-item/{id}:
+ *   get:
+ *     summary: Get order item by ID
+ *     description: Retrieve a specific order item by its unique ID.
+ *     tags:
+ *       - Order Item
+ *     parameters:
+ *       - name: orderId
+ *         in: path
+ *         required: true
+ *         description: UUID of the order to get order items of the order
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       200:
+ *         description: Order item retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   format: uuid
+ *                 name:
+ *                   type: string
+ *                   description: Name of the order item
+ *                 price:
+ *                   type: number
+ *                   description: Price of the order item
+ *                 quantity:
+ *                   type: number
+ *                   description: Quantity ordered
+ *       400:
+ *         description: Invalid ID supplied
+ *       404:
+ *         description: Order item not found
+ *       500:
+ *         description: Internal server error
+ */
 export async function getOrderItem(
   req: Request,
   res: Response,
